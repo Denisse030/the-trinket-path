c43d5111e961fec82152b58111e831a223333ed0
Denisse Benito Gutierrez <denisse@Denisses-MacBook-Pro.local>
2026-06-10 11:44:02 -0400

    Second Commit: Adding info to the json files

---
+++ src/pages/Galleries.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Galleries = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Galleries
---
+++ src/pages/Trinkets.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Trinkets = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Trinkets
