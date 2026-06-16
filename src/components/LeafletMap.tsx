f91ee838d7266d84c2907762ac904e26baec67f7
Denisse Benito Gutierrez <denisse@Mac.lan>
2026-06-16 05:06:47 -0400

    Eighth Commit: Fixing pins on map

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -1,6 +1,10 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import type { LatLngExpression } from "leaflet";
+import L, { type LatLngExpression } from "leaflet";
+import "leaflet/dist/leaflet.css";
 import "../App.css";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 type Place = {
   id: number;
@@ -13,31 +17,29 @@ type Place = {
   };
 };
 
-type LeafLetMapProps = {
+type LeafletMapProps = {
   places: Place[];
 };
 
-const LeafletMap = ({ places }: LeafLetMapProps) => {
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
+const LeafletMap = ({ places }: LeafletMapProps) => {
   const center: LatLngExpression = [40.73061, -73.935242];
 
   return (
     <div className="map-wrapper">
       <MapContainer center={center} zoom={11} className="leaflet-map">
-        <TileLayer
-          attribution="&copy; OpenStreetMap contributors"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+        <TileLayer attribution="&copy; OpenStreetMap contributors" url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
 
         {places.map((place) => (
-          <Marker
-            key={place.id}
-            position={[place.coordinates.lat, place.coordinates.lng]}
-          >
-            <Popup>
-              <strong>{place.name}</strong>
-              <br />
-              {place.address}
-            </Popup>
+          <Marker key={place.id} position={[place.coordinates.lat, place.coordinates.lng]}>
+            <Popup><strong>{place.name}</strong><br /> {place.address}</Popup>
           </Marker>
         ))}
       </MapContainer>
